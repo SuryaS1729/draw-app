cb917fe28feeb7c2a13a06ed853f4f9dffdb7eb1
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-01-31 09:33:22 +0530

    added schemas zod

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -2,12 +2,23 @@ import express from "express";
 import jwt from "jsonwebtoken";
 import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
-import {CreateUserSchema} from "@repo/common/types"
+import {CreateUserSchema, SignInSchema,CreateRoomSchema} from "@repo/common/types"
 
 const app = express();
 
 
+
 app.post("/signup",(req,res)=>{
+    const data= CreateUserSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
+
+
+
     //db call
 
     res.json({
@@ -16,6 +27,13 @@ app.post("/signup",(req,res)=>{
 
 })
 app.post("/signin",(req,res)=>{
+    const data= SignInSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
 
     const userId=1;
     const token= jwt.sign({
@@ -26,6 +44,13 @@ app.post("/signin",(req,res)=>{
     
 })
 app.post("/room",middleware,(req,res)=>{
+    const data= CreateRoomSchema.safeParse(req.body);
+    if(!data.success){
+        res.json({
+            message:"incorrect inputs"
+        })
+        return
+    }
 
     //db call
 
