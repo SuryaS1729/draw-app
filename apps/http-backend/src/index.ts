047897b367182a7d1418fde86abada6219d5ac19
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-01-31 09:24:54 +0530

    added code shareability

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-import { JWT_SECRET } from "./config";
+import { JWT_SECRET } from "@repo/backend-common/config";
 import { middleware } from "./middleware";
+import {CreateUserSchema} from "@repo/common/types"
 
 const app = express();
-app.get("/",(req,res)=>{
-    res.send("heya there")
-})
+
 
 app.post("/signup",(req,res)=>{
     //db call
