96f958527a0dcdb0f940c492b6d2eb4417830440
SuryaS1729 <surya.curiousfusion@gmail.com>
2025-02-04 18:45:25 +0530

    added the prisma push chat logic

--- apps/ws-backend/src/index.ts
+++ apps/ws-backend/src/index.ts
@@ -1,6 +1,7 @@
 import { WebSocket,WebSocketServer } from 'ws';
 import { JWT_SECRET } from '@repo/backend-common/config';
-import jwt, { JwtPayload } from "jsonwebtoken"
+import jwt, { JwtPayload } from "jsonwebtoken";
+import {prismaClient} from "@repo/db/client"
 
 const wss = new WebSocketServer({ port: 8080 });
 
@@ -26,7 +27,7 @@ try {
 } catch (e) {
   return null
 }
-return null
+
 }
 wss.on('connection', function connection(ws, request) {
   const url= request.url;
@@ -35,6 +36,7 @@ wss.on('connection', function connection(ws, request) {
   }
   const queryParams = new URLSearchParams(url.split("?")[1])
   const token = queryParams.get("token")|| ""
+
   const userId=checkUser(token)
 
  if(userId== null){
@@ -51,23 +53,39 @@ wss.on('connection', function connection(ws, request) {
  })
 
 
-  ws.on('message', function message(data) {
-    const parsedData = JSON.parse(data as unknown as string)
+  ws.on('message',async function message(data) {
+    let parsedData;
+    if (typeof data !== "string") {
+      parsedData = JSON.parse(data.toString());
+    } else {
+      parsedData = JSON.parse(data); // {type: "join-room", roomId: 1}
+    }
+
+
     if(parsedData.type == "join_room"){
       const user = users.find(x=>x.ws===ws)
       user?.rooms.push(parsedData.roomId)
       console.log(users)
     }
-    if(parsedData.type == "leave_room"){
+    if(parsedData.type === "leave_room"){
       const user = users.find(x=>x.ws===ws)
       if(!user){
         return
       }
       user.rooms=user?.rooms.filter(x=>x!==parsedData.roomId)
     }
+    // user.rooms=user?.rooms.filter(x=>x!==parsedData.roomId) once check later
     if(parsedData.type=="chat"){
       const roomId= parsedData.roomId;
       const message= parsedData.message;
+
+      await prismaClient.chat.create({
+        data:{
+          roomId,
+          message,
+          userId
+        }
+      });
 
       users.forEach(user=>{
         if(user.rooms.includes(roomId)){
